f5f61863621146b6eb83af84aad43befd49c1a3e
vanderleiks <vanderleiksilva@gmail.com>
2023-05-28 14:48:34 -0300

    add class OrderItemPK and OrderItem

--- src/main/java/com/system/dscommerce/entities/Order.java
+++ src/main/java/com/system/dscommerce/entities/Order.java
@@ -1,6 +1,10 @@
 package com.system.dscommerce.entities;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -10,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -31,6 +36,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
     }
@@ -73,6 +81,14 @@ public class Order {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> geProducts() {
+        return items.stream().map(x -> x.geProduct()).toList();
     }
 
 }
