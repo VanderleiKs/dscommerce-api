92daf07f2ccd443ba166441bc8377229584a08a6
vanderleiks <vanderleiksilva@gmail.com>
2023-09-17 11:12:45 -0300

    Add UserDatailsProjection and UserRepository

---
+++ src/main/java/com/system/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,8 @@
+package com.system.dscommerce.projections;
+
+public interface UserProjection {
+    String getUsername();
+    String getPassWord();
+    Long getRoleId();
+    String getAuthority();
+}
